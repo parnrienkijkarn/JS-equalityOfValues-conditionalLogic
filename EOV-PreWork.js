59c26dc63bf55c5fb816c23e55cc07eaa4e2a2fb
Paul Ren <pause.forever@gmail.com>
2022-09-05 23:08:56 +0700

    add answers, with some doubts, obviously

---
+++ EOV-PreWork.js
@@ -0,0 +1,15 @@
+Object.is("Twenty", 20); // False
+Object.is({ name: "John" }, { name: "John" }); // True
+Object.is(true, 1); // False
+NaN == NaN; // FALSE
+10 == "10"; // True
+true == 1;  // TRUE
+0 === -0;  // TRUE
+null === null; // TRUE
+true === 1; // False
+true == "true"; // FALSE
+null == undefined; // True
+0 != ""; // FALSE
+[1, 2] == "1,2"; // True
+
+// got questions for number 4, 8, 10, 12
--- EOV-ex-3.js
+++ EOV-ex-3.js
@@ -1,12 +1,24 @@
 function isPlainObject(obj) {
-  // Start coding here
+  // อันนี้คืออันที่เคยทำกับพี่พันแต่เสือกจำไม่ได้ 55555
+  return typeof(obj) === "object" && obj !== null && !Array.isArray(obj);
 }
+
+// อันนี้เอามาจากพี่หมูกับพี่จอช
+// function isPlainObject(obj) {
+// let a = (typeof(obj) === "object");
+// let b =!Array.isArray(obj);
+// let c = (obj !== null);
+// let d = a && b && c;
+// return a, b, c, d
+// }
 
 
 // Example case
 let result1 = isPlainObject({ name: "John" });
 let result2 = isPlainObject(null);
 let result3 = isPlainObject(["apples", "oranges"]);
+
+
 
 // ผลลัพธ์ควรที่ได้จาก Example case
 console.log(result1); //true
